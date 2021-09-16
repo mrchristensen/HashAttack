867f5f0e3e46c220f1abe244d2abf007cf168c1d
Matt <mr.christensen@gmail.com>
2021-09-16 14:23:18 -0600

    my_sha_1 now prints in binary and truncates

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import hashlib
 # print(f'Hi, {name}')
 
+number_or_bits_for_tests = {8, 12, 16, 24, 32}
 
-def my_sha_1(string_to_hash, num_bits):
-    digest = hashlib.sha1(string_to_hash.encode('utf-8')).hexdigest()
-    digest_truncated = digest  # todo: truncate the digest to the num of bits
+def my_sha_1(string_to_hash, num_bits):  # https://www.kite.com/python/examples/3150/hashlib-construct-a-sha1-hash
+    hex_digest = hashlib.sha1(string_to_hash.encode('utf-8')).hexdigest()
+    bin_digest = bin(int(hex_digest, base=16)).lstrip('0b')
+    digest_truncated = bin_digest[0:num_bits]
     return digest_truncated
 
 
