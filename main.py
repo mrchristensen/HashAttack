4b705a2bdd9593df2d2944499639580eb0fdaddc
Matt <mr.christensen@gmail.com>
2021-09-16 13:53:09 -0600

    SHA-1 wrapper without truncation

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import hashlib
+# print(f'Hi, {name}')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def my_sha_1(string_to_hash, num_bits):
+    digest = hashlib.sha1(string_to_hash.encode('utf-8')).hexdigest()[0:num_bits/8]
+    digest_truncated = digest  # todo: truncate the digest to the num of bits
+    return digest_truncated
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    string_to_hash = input("String to hash: ")
+    num_bits = int(input("Num bits: "))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(my_sha_1(string_to_hash, num_bits))
