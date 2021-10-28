ef75d292be713a190472e325ecd515653b0acc4f
Matt Christensen <mr.christensen@gmail.com>
2021-10-27 23:55:56 -0600

    Fixed printing issues

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import seaborn as sns
 # num_of_bits_for_tests = [8, 10, 16, 20, 24]
 num_of_bits_for_tests = [8, 10, 16, 20]
 # num_of_bits_for_tests = [8, 10]
-num_of_test_iter = 50
+# num_of_bits_for_tests = [8]
+num_of_test_iter = 100
 
 
 def make_box_plot(results, filename, title):
@@ -34,7 +35,7 @@ def make_line_plot(results, filename, max, title, mode):
     for num_bits in results:
         x.append(num_bits)
         y.append(sum(results[num_bits]) / len(results[num_bits]))
-    plt.plot(x, y, linestyle='None', marker="o")
+    plt.plot(x, y, linestyle='None', marker="o", label="Average Number of Attempts")
     x = np.linspace(6, max, 100)
 
     y_equation = None
@@ -43,8 +44,9 @@ def make_line_plot(results, filename, max, title, mode):
     else:
         y_equation = 2 ** x
 
-    plt.plot(x, y_equation, 'r')
+    plt.plot(x, y_equation, 'r', label="Theoretical Probability")
     plt.title(title)
+    plt.legend(loc="upper left")
     plt.xlabel("Number of Bytes")
     plt.ylabel("Attempts")
     plt.yscale("log")
@@ -64,6 +66,7 @@ def my_sha_1(string_to_hash, num_bits):  # https://www.kite.com/python/examples/
 
 def run_collision_attack_test():
     results = {}
+    print('collision_attack')
 
     for num_bits in num_of_bits_for_tests:
         # print(f'num_bits: {num_bits}')
@@ -82,28 +85,31 @@ def run_collision_attack_test():
 
     make_line_plot(results, "PlotCol", 26, "Average Attempts for Collision Attack", "collision")
     make_box_plot(results, "BoxCol", "Variation in Attempts for Collision Attack")
+    # print(f'results: {results}')
 
 
 def run_pre_image_attack_test():
     results = {}
+    print('pre_image_attack')
 
     for num_bits in num_of_bits_for_tests:
         # print(f'num_bits: {num_bits}')
         num_attempts = set()
 
         for i in range(num_of_test_iter):
-            print(f'i: {i}')
+            # print(f'i: {i}')
             num_attempts.add(pre_image_attack(num_bits))
 
         results[num_bits] = num_attempts
         print(f'Attempts for {num_bits}: {num_attempts}'
               f'\nAverage num attempts: {sum(num_attempts) / len(num_attempts)}'
-              f'\nExpected num of attempts: {2 ** (num_bits / 2)}'
+              f'\nExpected num of attempts: {2 ** num_bits}'
               f'\nLargest Value: {max(num_attempts)}'
               f'\nSmallest Value: {min(num_attempts)}\n\n')
 
     make_line_plot(results, "PlotPre", 26, "Average Attempts for Pre-Image Attack", "pre-image")
     make_box_plot(results, "BoxPre", "Variation in Attempts for Pre-Image Attack")
+    # print(f'results: {results}')
 
 
 def collision_attack(num_bits):
